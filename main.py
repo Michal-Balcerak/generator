d3f65565cb2ad537c8e89f7aaa9950e0156b07dd
michal <michal.balcerak28@gmail.com>
2025-07-30 11:40:08 +0200

    Poprawne odczytywanie portu z ENV

--- main.py
+++ main.py
@@ -27,4 +27,5 @@ def generate():
         return "PDF nie został wygenerowany", 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
